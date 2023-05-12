dc8975a29d60eb2739172cd1f5cea1e4845b9cb2
@ ANTU <sajalhowlader08@gmail.com>
2023-05-12 10:29:32 +0600

    add server link

--- src/Config.js
+++ src/Config.js
@@ -1,3 +1,3 @@
 export const BASE_API =
-  "https://handy-helper-server-site-cww0m6i0g-antu13az.vercel.app/";
+  "https://handy-helper-server.vercel.app";
 // export const BASE_API = "http://localhost:5000";
